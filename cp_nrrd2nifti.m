f66c6416269c9367967224d493de7754b1959eeb
Christophe Phillips <c.phillips@ulg.ac.be>
2016-05-10 12:57:55 +0200

    Adding main routine
    No code there yet, jus setting up the stage!

---
+++ cp_nrrd2nifti.m
@@ -0,0 +1,16 @@
+function Pout = cr_nrrd2nifti(Pin,opt)
+% Function to convert one (or more) NRRD image(s) into NIFTI formt.
+% 
+% First the header and data from the NRRD images is read in then is written
+% into a (.nii) Nifti file. This relies on the NRRD reader functions from 
+% Gregory Jefferis (https://github.com/jefferis) and SPM12.
+%_______________________________________________________________________
+% Copyright (C) 2016 Cyclotron Research Centre
+
+% Written by C. Phillips.
+% Cyclotron Research Centre & % GIGA in silico medicine
+% University of Liege, Belgium
+
+
+
+end
